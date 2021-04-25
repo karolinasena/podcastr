e149ab1437b7c3bfbda5c18036b79da68ed1fc1d
Karolina Sena <ana.sena@driven.cx>
2021-04-25 12:38:47 -0300

    adiciona scroll na página episode

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -29,36 +29,38 @@ export default function Episode({ episode }: EpisodeProps) {
   const { play } = usePlayer();
 
   return (
-    <div className={styles.episode}>
-      <Head>
-        <title>{episode.title}</title>
-      </Head>
-      <div className={styles.thumbnailContainer}>
-        <Link href="/">
-          <button type="button">
-            <img src="/arrow-left.svg" alt="Voltar" />
+    <div className={styles.episodeContainer}>
+      <div className={styles.episode}>
+        <Head>
+          <title>{episode.title}</title>
+        </Head>
+        <div className={styles.thumbnailContainer}>
+          <Link href="/">
+            <button type="button">
+              <img src="/arrow-left.svg" alt="Voltar" />
+            </button>
+          </Link>
+          <Image
+            width={700}
+            height={160}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <button type="button" onClick={() => play(episode)}>
+            <img src="/play.svg" alt="Tocar episódio" />
           </button>
-        </Link>
-        <Image
-          width={700}
-          height={160}
-          src={episode.thumbnail}
-          objectFit="cover"
-        />
-        <button type="button" onClick={() => play(episode)}>
-          <img src="/play.svg" alt="Tocar episódio" />
-        </button>
+        </div>
+
+        <header>
+          <h1>{episode.title}</h1>
+          <span>{episode.members}</span>
+          <span>{episode.publishedAt}</span>
+          <span>{episode.durationAsString}</span>
+        </header>
+
+        <div className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }} />
+
       </div>
-
-      <header>
-        <h1>{episode.title}</h1>
-        <span>{episode.members}</span>
-        <span>{episode.publishedAt}</span>
-        <span>{episode.durationAsString}</span>
-      </header>
-
-      <div className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }} />
-
     </div>
   )
 }
